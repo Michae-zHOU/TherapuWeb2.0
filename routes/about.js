830414d72e2bf8806e0eb6db52ae2ca1bac81de2
mahaoran <mahaoran1020@gmail.com>
2017-09-12 20:59:03 -0400

    add footer files

--- routes/about.js
+++ routes/about.js
@@ -22,7 +22,54 @@ router.get('/', function(req, res, next) {
         }, 
     });
 })
-
+router.get('/waiver', function(req, res, next) {
+    res.render('waiver', {
+        partials: {
+            header: '../views/partials/header',
+            head: '../views/partials/head',
+            scripts: '../views/partials/scripts',
+            footer: '../views/partials/footer'
+            },
+        title: '法律声明',
+        auth: function() {
+            if (req.user) {
+                return req.user.admin
+            }
+        }, 
+    })
+})
+router.get('/copyright', function(req, res, next) {
+    res.render('copyright', {
+        partials: {
+            header: '../views/partials/header',
+            head: '../views/partials/head',
+            scripts: '../views/partials/scripts',
+            footer: '../views/partials/footer'
+            },
+        title: '版权及免责声明',
+        auth: function() {
+            if (req.user) {
+                return req.user.admin
+            }
+        }, 
+    })
+})
+router.get('/privacy', function(req, res, next) {
+    res.render('privacy', {
+        partials: {
+            header: '../views/partials/header',
+            head: '../views/partials/head',
+            scripts: '../views/partials/scripts',
+            footer: '../views/partials/footer'
+            },
+        title: '隐私权保护声明',
+        auth: function() {
+            if (req.user) {
+                return req.user.admin
+            }
+        }, 
+    })
+})
 
 
 module.exports = router;
